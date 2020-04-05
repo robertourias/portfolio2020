ecdf2a2be663f2852a3266ba051d3d39f635d559
Roberto Nicoletti <roberto.urias@gmail.com>
2020-04-05 18:08:10 -0300

    Iniciando seção de projetos

--- src/app/sections/projects/projects.component.ts
+++ src/app/sections/projects/projects.component.ts
@@ -6,10 +6,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent implements OnInit {
+  public cards: Array<{
+    thumb: string;
+    name: string;
+    description: string;
+    key: string;
+  }>;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.cards = [
+      {
+        thumb: 'uolhost.jpg',
+        name: 'UOLHost',
+        description: `Site que simula a interação do usuário com a câmera do LG G3 usando a webcam. O projeto foi desenvolvido na Huia/W3haus
+        e atuei como Desenvolvedor front-end.`,
+        key: 'HTML 5, CSS 3, Javascript, Angular 8'
+      },
+      {
+        thumb: 'uolhost.jpg',
+        name: 'UOLHost',
+        description: `Site que simula a interação do usuário com a câmera do LG G3 usando a webcam. O projeto foi desenvolvido na Huia/W3haus
+        e atuei como Desenvolvedor front-end.`,
+        key: 'HTML 5, CSS 3, Javascript, Angular 8'
+      },
+      {
+        thumb: 'uolhost.jpg',
+        name: 'UOLHost',
+        description: `Site que simula a interação do usuário com a câmera do LG G3 usando a webcam. O projeto foi desenvolvido na Huia/W3haus
+        e atuei como Desenvolvedor front-end.`,
+        key: 'HTML 5, CSS 3, Javascript, Angular 8'
+      },
+      {
+        thumb: 'uolhost.jpg',
+        name: 'UOLHost',
+        description: `Site que simula a interação do usuário com a câmera do LG G3 usando a webcam. O projeto foi desenvolvido na Huia/W3haus
+        e atuei como Desenvolvedor front-end.`,
+        key: 'HTML 5, CSS 3, Javascript, Angular 8'
+      },
+      {
+        thumb: 'uolhost.jpg',
+        name: 'UOLHost',
+        description: `Site que simula a interação do usuário com a câmera do LG G3 usando a webcam. O projeto foi desenvolvido na Huia/W3haus
+        e atuei como Desenvolvedor front-end.`,
+        key: 'HTML 5, CSS 3, Javascript, Angular 8'
+      },
+      {
+        thumb: 'uolhost.jpg',
+        name: 'UOLHost',
+        description: `Site que simula a interação do usuário com a câmera do LG G3 usando a webcam. O projeto foi desenvolvido na Huia/W3haus
+        e atuei como Desenvolvedor front-end.`,
+        key: 'HTML 5, CSS 3, Javascript, Angular 8'
+      }
+    ];
   }
-
 }
