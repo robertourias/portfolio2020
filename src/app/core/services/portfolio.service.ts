a58d2deb5c549a33baaad9710d8ee1680c280c1f
Roberto Nicoletti <roberto.urias@gmail.com>
2020-04-07 21:37:50 -0300

    Criando serviço com dados do portfolio

--- src/app/core/services/portfolio.service.ts
+++ src/app/core/services/portfolio.service.ts
@@ -4,6 +4,28 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class PortfolioService {
+  public static NAME = 'Roberto Nicoletti';
+  public static YEARSOLD = 32;
+  public static TIMEEXPERIENCE = 10;
 
   constructor() { }
+
+  getSkills(): Array<{name: string, score: number}> {
+    const skills = [
+      { name: 'HTML 5', score: 9.5 },
+      { name: 'CSS 3', score: 9.5 },
+      { name: 'Javascript', score: 9 },
+      { name: 'Angular 9', score: 8.5 }
+    ];
+
+    return skills;
+  }
+
+  getProjects(): Array<{name: string, thumb: string}> {
+    const projects = [
+      { name: 'UOL Host', thumb: 'img.jpg'}
+    ];
+
+    return projects;
+  }
 }
